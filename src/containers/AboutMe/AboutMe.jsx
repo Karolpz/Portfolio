2678b2290c3c1aed6396034175f06e79f00215ef
Karolpz <carolinelpz34@gmail.com>
2025-04-17 08:47:26 +0200

    add rich snippets

--- src/containers/AboutMe/AboutMe.jsx
+++ src/containers/AboutMe/AboutMe.jsx
@@ -4,10 +4,10 @@ import Portrait from '../../assets/images/pictures/portrait-caroline-lopez-IA.we
 
 const AboutMe = () => {
     return (
-        <section className='aboutMe'id='aboutMe'>
+        <section className='aboutMe' id='aboutMe'>
             <img src={Portrait} alt='Portrait de Caroline Lopez' className="aboutMe__pic" />
-            <div className="aboutMe__text">
-                <h1>Bonjour, je suis Caroline Lopez, développeuse front-end junior, bienvenue dans mon univers !</h1>
+            <div className="aboutMe__text" itemscope itemtype="https://schema.org/Person">
+                <h1>Bonjour, je suis <span itemprop="givenName">Caroline</span> <span itemprop="familyName" >Lopez</span>, développeuse front-end junior près de <span itemprop="homeLocation">Lyon</span>, bienvenue dans mon univers !</h1>
                 <p>J'ai suivi une formation <span>d'intégratrice web</span> sur Openclassrooms, qui m'a permis d'acquérir des compétences afin de développer des sites dynamiques et responsives. J'accorde une importance particulière sur <span>l'accessibilité</span>. Chaque utilisateur mérite une expérience fluide et agréable, mon but est de rendre les sites les plus inclusifs possibles.</p><p>Le developpement web me permet d'assouvir ma curiosité et de m'exprimer à travers mes projets. Je vous invite donc à voyager à travers mon portfolio, et à me contacter si vous souhaitez discuter ou échanger sur un futur projet.</p>
             </div>
         </section>
--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import Lottie from 'lottie-react'
 import rocketAnimation from '../../data/rocket.json'
+import Location from '../../assets/images/logos/location-logo.webp'
 
 
 const Footer = () => {
@@ -13,16 +14,23 @@ const Footer = () => {
     window.scrollTo({ top: 0, behavior: "smooth" })
   }
   return (
-    <footer className='footer'>
+    <footer itemscope itemtype="https://schema.org/Person" className='footer'>
       <div className="linkRS">
         <a href='https://github.com/Karolpz' target="_blank" rel="noopener noreferrer" aria-label="lien vers la page github de Caroline Lopez">
           <FontAwesomeIcon icon={faGithub} className='linkRS__github' />
         </a>
         <a href='https://www.linkedin.com/in/caroline-l-435a11b5/' target="_blank" rel="noopener noreferrer" aria-label="lien vers la linkedin de Caroline Lopez">
-        <FontAwesomeIcon icon={faLinkedin} className='linkRS__linkedin' />
+          <FontAwesomeIcon icon={faLinkedin} className='linkRS__linkedin' />
         </a>
+        <div className='location'>
+          <img src={Location} alt='Logo de la localisation géographique de Caroline Lopez' />
+          <p>
+            <span itemprop="addressLocality">Bourgoin-Jallieu</span>,
+            <span itemprop="postalCode">38300</span>
+          </p>
+        </div>
       </div>
-      <p>© 2025 - Caroline Lopez. Tous droits réservés.</p>
+      <p className='text'>© 2025 - Caroline Lopez. Tous droits réservés.</p>
       <Lottie animationData={rocketAnimation} loop={true} className='rocketAnimation' onClick={handleScrollTop} />
     </footer>
   )
