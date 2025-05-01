21cfcac9f90078528fe88e91f20117b80e7bfde4
Karolpz <carolinelpz34@gmail.com>
2025-05-01 18:48:37 +0200

    test perf

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,21 +1,21 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react"
 
-export const ThemeContext = createContext();
+export const ThemeContext = createContext()
 
 export const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState("dark")
 
     const toggleTheme = () => {
-        setTheme(theme === "dark" ? "light" : "dark");
+        setTheme(theme === "dark" ? "light" : "dark")
     };
 
     useEffect(() => {
-        document.body.className = theme;
-    }, [theme]);
+        document.body.className = theme
+    }, [theme])
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             {children}
         </ThemeContext.Provider>
-    );
-};
+    )
+}
